e59ee6ff30bc84e6edfa50e2f47e6c78afad5586
Hoyoung Lee <99785007+hoyoungleee@users.noreply.github.com>
2025-03-24 11:18:26 +0900

    패키지 수정

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-import common.input.Input;
+import common.Input;
 
-import static common.input.Input.inputInt;
+import static common.Input.inputInt;
 
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
